543701fe1ca179f75ccee18ffdf9cb71cc408848
Ubuntu <ubuntu@ip-172-31-30-203.ec2.internal>
2022-05-25 15:46:38 +0000

    database update

--- config/db-config.js
+++ config/db-config.js
@@ -1,7 +1,7 @@
 
 const Sequelize = require('sequelize');
-const db = new Sequelize('fooddb','root','mypass', {
-    host:'localhost',
+const db = new Sequelize('fooddb','admin','fooddb2601', {
+    host:'fooddb.c33vfgmfefmv.us-east-1.rds.amazonaws.com',
     dialect:'mysql',
     define:{
         timestamps: false,
